c2f9ac0a94029bf3487a9112d88ede3a03108809
hector yegre <programadoryegre@gmail.com>
2026-01-17 10:13:57 -0400

    feat: se crea presentation / socket autenticacion

---
+++ apps/backend/src/presentation/sockets/socketAuth.ts
@@ -0,0 +1,30 @@
+import { Socket } from "socket.io";
+import jwt from "jsonwebtoken";
+import { UserRole } from "../../domain/enums/UserRole";
+
+
+interface Payload {
+    id:string,
+    email:string,
+    role:UserRole
+}
+export const socketAuth = (socket:Socket, next:(err?:Error) => void)=>{
+    try {
+        const token = socket.handshake.auth?.token;
+        if(!token){
+            return next(new Error("No existe tojen"))
+        }
+
+        const payload = jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        ) as Payload
+
+        socket.data.user = payload // guardamos el usuario conectado
+        next();
+        
+    } catch (error) {
+        next(new Error("Token invalido"));
+
+    }
+}
