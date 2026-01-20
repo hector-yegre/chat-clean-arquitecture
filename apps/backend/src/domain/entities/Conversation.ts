dccb58ef4375947e20720ff97c7a79098908f217
hector yegre <programadoryegre@gmail.com>
2026-01-19 22:42:10 -0400

    fix se agregan tipos a la entidad de conversation

--- apps/backend/src/domain/entities/Conversation.ts
+++ apps/backend/src/domain/entities/Conversation.ts
@@ -1,29 +1,52 @@
 import { UserRole } from "../enums/UserRole";
 
+interface Participants {
+    userId: string;
+    role: UserRole;
+}
+
+
+interface Message {
+    emisorId: string;
+    content: string;
+    createdAt: Date;
+}
 
 export class Conversation {
 
     public readonly id: string;
     public readonly isGroup: boolean;
-    public readonly name?: string;
-    public readonly participants: { userId: string; role: UserRole }[];
-    public readonly messages: { senderId: string; content: string; createdAt: Date }[];
+    public readonly name: string;
+    public readonly participants: Participants[];
+    public readonly messages: Message[];
     public readonly createdAt: Date;
 
     constructor( params:{
         id?: string;
         isGroup: boolean;
-        name?: string;
+        name: string;
         participants: { userId: string; role: UserRole }[];
-        messages?: { senderId: string; content: string; createdAt: Date }[];
+        messages?: { emisorId: string; content: string; createdAt: Date }[];
         createdAt?: Date;
     }){
-        this.id = params.id ?? crypto.randomUUID(); // de la porpia capa de dominio uuid
+        this.id = params.id ?? crypto.randomUUID(); // de la propia capa de dominio uuid
         this.isGroup = params.isGroup;
-        this.name = params.name;
+        this.name = params.name ?? 'private';
         this.participants = params.participants;
         this.messages = params.messages ?? [];
         this.createdAt = params.createdAt ?? new Date();
+    }
+
+    getParticipantIds(): string[] {
+        return this.participants.map(participant => participant.userId);
+    }
+
+    getConversationName(): string {
+        if (this.isGroup) {
+            return this.name ?? "Unnamed Group";
+        } else {
+            return "Direct Message";
+        }
     }
 
 }
