a732d05ee81d74750fd54d1025da4d832088f770
hector yegre <programadoryegre@gmail.com>
2026-01-19 22:43:27 -0400

    fix se ajusta shema de conversation

--- apps/backend/src/infrastructure/models/conversationModel.ts
+++ apps/backend/src/infrastructure/models/conversationModel.ts
@@ -1,16 +1,12 @@
-
 import mongoose from 'mongoose'
-import { required } from 'zod/v4/core/util.cjs'
-
-
 
 const conversationSchema = new mongoose.Schema({
 
-    idGroup: { type: Boolean, default: false},
+    isGroup: { type: Boolean, default: false},
     name: {type:String}, // para grupos 
     participants: [{
-        user:{type: mongoose.Types.ObjectId, ref: "User", require:true},
-        role:{ type:String, enum:["ADMIN","USER"],require:true}
+        user:{type: mongoose.Types.ObjectId, ref: "User", required:true},
+        role:{ type:String, enum:["ADMIN","USER"],required:true}
     }],
     messages:[{
         emisor: {type: mongoose.Types.ObjectId, ref: "User", required: true},
